6584904b2a67f0d60fdd289980e3885251736399
Dylan Lott <dylan@storj.io>
2017-04-27 12:54:48 -0600

    Added storj.js, more setup

--- app/src/renderer/routes.js
+++ app/src/renderer/routes.js
@@ -5,6 +5,16 @@ export default [
     component: require('components/LandingPageView')
   },
   {
+    path: '/buckets',
+    name: 'buckets',
+    component: require('components/BucketsView')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: require('components/DashboardView')
+  },
+  {
     path: '*',
     redirect: '/'
   }
